c2d15d76c865a93c9592ce18da9a392fbe35defe
Radu Dumitrescu <d.raduandrei@gmail.com>
2024-06-25 16:18:19 +0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -3,7 +3,7 @@
 export const handler = async (event) => {
   console.log("Function was called");
   console.log(event);
-  const name = event.queryStringParameters?.name || "John";
+  const name = event.queryStringParameters?.name || "World";
   return {
     statusCode: 200,
     body: `Hello, ${name}! Welcome to Genezio Functions!`,
